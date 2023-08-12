5cbaf40df0965322d6b8b03c1500c6bb5294ee96
Shane <sb_94@yahoo.com>
2023-08-12 11:02:49 -0700

    added index.js file to root

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to your MongoDB database
+mongoose.connect('mongodb://localhost:27017/your-database-name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+// Middleware
+app.use(express.json());
+
+// Use your routes
+const routes = require('./routes');
+app.use(routes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
