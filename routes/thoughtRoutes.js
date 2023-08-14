46d88609bdcffecdfdf52421f3ae64b4e3c45ee4
SixFourDev <sb_94@yahoo.com>
2023-08-13 18:51:52 -0700

    added to thoughtRoutes.js

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -8,6 +8,11 @@ const {
     deleteThought
 } = require('../controllers/thoughtController');
 
+const {
+    createReaction,
+    deleteReaction
+} = require('../controllers/reactionController');
+
 // GET route to get all thoughts
 router.get('/', getAllThoughts);
 
@@ -22,5 +27,11 @@ router.put('/:thoughtId', updateThought);
 
 // DELETE route to remove a thought by its _id
 router.delete('/:thoughtId', deleteThought);
+
+// POST route to add a reaction to a thought
+router.post('/:thoughtId/reactions', createReaction);
+
+// DELETE route to remove a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
 
 module.exports = router;
